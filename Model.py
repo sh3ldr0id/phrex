aa99342555317f74dfb9268d27c12c2f8e775ef6
Sheldon Sinu <sh3ldr0id@gmail.com>
2023-11-02 12:12:40 +0530

    dataset processing

--- Model.py
+++ Model.py
@@ -2,42 +2,83 @@ import numpy as np
 from tensorflow import keras
 from keras import layers
 from keras.datasets import mnist
-from keras.utils import to_categorical
+from os import listdir
+from cv2 import imread, resize
+from tqdm import tqdm
+from random import randint, sample, choice
 
-# Load and preprocess the MNIST dataset
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
-X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
-# y_train = to_categorical(y_train, 10)
-# y_test = to_categorical(y_test, 10)
+# Load Dataset
+dataset = []
 
-# Define the CNN model
+folders = listdir("dataset")[:10]
+
+for folder in tqdm(folders):
+    files = listdir(f"dataset/{folder}")
+
+    images = []
+
+    for file in files:
+        image = imread(f"dataset/{folder}/{file}", 0)
+
+        image = resize(image, (100, 100))
+
+        images.append(image)
+
+    dataset.append(images)
+arr = np.array(dataset, dtype=np.uint8)
+
+memory_usage = round(arr.nbytes / (1024 * 1024), 2)
+print(f"{memory_usage} MB Used")
+
+# Process Dataset
+def getRandom(x):
+    index = x
+
+    while x == index:
+        index = randint(0, len(dataset))
+
+    return index
+
+features = []
+labels = []
+
+for index in range(len(dataset)):
+    data = dataset[index]
+
+    for i in range(4):
+        shuffled = sample(data, len(data))
+
+        features.append(shuffled[:-63])
+
+        if i % 2 == 0:
+            feature = choice(shuffled)
+
+            labels.append(1)
+
+        else:
+            feature = choice(dataset[getRandom(index)])
+
+            labels.append(0)
+
+        features[-1].append(feature)
+        
+# Build Model
 model = keras.Sequential()
 
-# Add an Input layer with the shape (28, 28, 1)
 model.add(layers.Input(shape=(28, 28, 1)))
 
-# Convolutional layers
 model.add(layers.Conv2D(32, kernel_size=(3, 3), activation='relu'))
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 model.add(layers.Conv2D(64, kernel_size=(6, 6), activation='relu'))
 model.add(layers.MaxPooling2D(pool_size=(4, 4)))
 
-# Flatten layer
 model.add(layers.Flatten())
 
-# Fully connected layers
 model.add(layers.Dense(128, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
 
-# Compile the model
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Print a summary of the model
 model.summary()
 
 # Train the model
